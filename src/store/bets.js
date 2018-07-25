117c1d4079ee994a1912cb9e421c0cc5d3381b41
Jeremy Grubard <jgrubard@gmail.com>
2018-07-25 16:49:30 -0400

    commit

--- src/store/bets.js
+++ src/store/bets.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 
 const GET_BETS = 'GET_BETS';
 const CREATE_BET = 'CREATE_BET';
+const UPDATE_BET = 'UPDATE_BET';
 
 const getBets = (bets) => ({ type: GET_BETS, bets });
 const createBet = (bet) => ({ type: CREATE_BET, bet });
+const updateBet = (bet) => ({ type: UPDATE_BET, bet });
 
 export const getBetsFromServer = () => {
   return dispatch => {
@@ -22,22 +24,27 @@ export const createBetOnServer = (bet) => {
   }
 }
 
-// export const updateBetOnServer = (bet) => {
-//   return dispatch => {
-//     return axios.post(`/api/bets/${bet.id}`, bet)
-//       .then(res => res.data)
-//       .then(bet => dispatch(createBets(bet)))
-//   }
-// }
+export const updateBetOnServer = (bet) => {
+  const{ user1Id, user2Id } = bet;
+  return dispatch => {
+    return axios.post('/accept', { user1Id, user2Id })
+      .then(res => res.data)
+      .then(bet => dispatch(updateBet(bet)))
+  }
+}
 
 const store = (state = [], action) => {
+  let bets;
   switch(action.type) {
-  case GET_BETS:
-    return action.bets;
-  case CREATE_BET:
-    return [ ...state, action.bet ];
-  default:
-    return state;
+    case GET_BETS:
+      return action.bets;
+    case CREATE_BET:
+      return [ ...state, action.bet ];
+    case UPDATE_BET:
+      bets = state.filter(bet => bet.id === action.bet.id)
+      return [ ...bets, action.bet ];
+    default:
+      return state;
   }
 }
 
