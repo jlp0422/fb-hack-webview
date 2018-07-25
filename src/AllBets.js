1cee91bf749fa9eec2c15482e354f308b79d016a
Jeremy Grubard <jgrubard@gmail.com>
2018-07-25 16:32:44 -0400

    commit

--- src/AllBets.js
+++ src/AllBets.js
@@ -1,7 +1,53 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class AllBets extends React.Component {
   constructor() {
     super();
   }
+
+  onAddUserToBet(id, bet) {
+    const { id, stake, wager }
+  }
+
+  render() {
+    const { onAddUserToBet } = this;
+    const { bets, users, user } = this.props;
+    console.log(bets);
+    return (
+      <div>
+        <h2>All Bets</h2>
+        <div>
+          {
+            bets.map(bet => {
+              const user1 = users.find(user => user.facebookId === bet.userOneFacebookId);
+              return (
+                <div key={bet.id}>
+                  {bet.wager} {bet.stake} {user1.first_name} {user1.last_name}
+                  <button onClick={() => onAddUserToBet(user.facebookId, bet)}>Take Bet!</button>
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
+    );
+  }
 }
+
+const mapState = ({ user, users, bets }) => {
+  console.log(bets);
+  return {
+    bets,
+    users,
+    user
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    updateBet: (bet) => dispatch(updateBetOnServer(bet))
+  }
+}
+
+export default connect(mapState, mapDispatch)(AllBets);
